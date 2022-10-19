8eb229d1703e83a7ce0ae778648752190f68b014
nish <nishant5800@gmail.com>
2022-10-19 05:20:17 +0200

    Apply cosmetic changes to labels.

--- CoolblueApp/ViewModels/ProductItemViewModel.swift
+++ CoolblueApp/ViewModels/ProductItemViewModel.swift
@@ -15,10 +15,16 @@ class ProductItemViewModel {
     var formattedReview: NSAttributedString {
         let filledStar = NSTextAttachment()
         let imageConfig = UIImage.SymbolConfiguration(scale: .small)
-        let image = UIImage(systemName: "star.circle.fill")?.withTintColor(.systemGreen)
-        filledStar.image = image?.withConfiguration(imageConfig)
+        guard let image = UIImage(systemName: "star.circle.fill")?.withTintColor(.systemGreen),
+              let font = UIFont(name: "HelveticaNeue", size: 14.0) else {
+                  return NSAttributedString()
+        }
+        
+        filledStar.image = image.withConfiguration(imageConfig)
+        filledStar.bounds = CGRect(x: 0.0, y: (font.capHeight - image.size.height)/2, width: image.size.width, height: image.size.height)
+        
         let attributedString = NSMutableAttributedString(attachment: filledStar)
-        let descriptionString = " \(product.reviewInformation.reviewSummary.reviewAverage) (\(product.reviewInformation.reviewSummary.reviewCount)) reviews"
+        let descriptionString = " \(product.reviewInformation.reviewSummary.reviewAverage) (\(product.reviewInformation.reviewSummary.reviewCount) reviews)"
         attributedString.append(NSAttributedString(string: descriptionString))
         return attributedString
     }
@@ -26,8 +32,14 @@ class ProductItemViewModel {
     var formattedUSPs: NSAttributedString {
         let bulletPoint = NSTextAttachment()
         let imageConfig = UIImage.SymbolConfiguration(scale: .small)
-        let image = UIImage(systemName: "checkmark.circle")?.withTintColor(.systemGray)
-        bulletPoint.image = image?.withConfiguration(imageConfig)
+        guard let image = UIImage(systemName: "checkmark.circle")?.withTintColor(.systemGray),
+              let font = UIFont(name: "HelveticaNeue", size: 14.0) else {
+                  return NSAttributedString()
+        }
+        
+        bulletPoint.image = image.withConfiguration(imageConfig)
+        bulletPoint.bounds = CGRect(x: 0.0, y: (font.capHeight - image.size.height)/2, width: image.size.width, height: image.size.height)
+        
         let attributedString = NSMutableAttributedString(string: "")
         for usp in product.USPs {
             attributedString.append(NSAttributedString(attachment: bulletPoint))
